902bc78f1534a66ea96a9d9de6aefa7a8ee69aa4
Jimmy Svejderud <jimmy@svejderud.se>
2018-04-29 19:50:02 +0200

    Round 1B: Added problem texts

--- Round1B/Problem2/Solution.cs
+++ Round1B/Problem2/Solution.cs
@@ -6,6 +6,23 @@ using System.Threading.Tasks;
 
 public class Solution /* Problem 2 */
 {
+    /*
+     Mysterious Road Signs
+    Problem
+
+    The town of Signfield is on a perfectly straight and infinitely long road running from west to east. Along that road, there is a sequence of S mysterious road signs with numbers on both sides. The i-th sign (numbered in order from west to east) is at a point Di kilometers east of Signfield, and has a number Ai on the west-facing side and a number Bi on the east-facing side.
+
+    Nobody in Signfield knows what these signs are trying to say. You think that the numbers on the west sides of the signs are intended for drivers traveling east, and that they represent distances to some particular destination. Similarly, you think that the numbers on the east sides of the signs are for drivers traveling west, and that they represent distances to some particular destination. You suspect that not all of the signs may be consistent with this theory, though.
+
+    To start testing your theory, you are interested in finding valid sets of signs that obey the following rules:
+
+        The set is a contiguous subsequence of the sequence of all road signs. (The entire sequence also counts as a contiguous subsequence.)
+        There exist locations M and N kilometers east of Signfield, where M and N are (not necessarily positive and not necessarily distinct) numbers, such that for every sign in that set, at least one of the following is true:
+            Di + Ai = M.
+            Di - Bi = N.
+
+    What is the largest possible number of signs in a valid set as described above, and how many different valid sets of that size are there? 
+     */
     public static void Main(string[] args)
     {
         var totalTests = int.Parse(Console.ReadLine());
--- Round1B/Problem3/Solution.cs
+++ Round1B/Problem3/Solution.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 public class Solution /* Problem 3 */
 {
+    /*
+     Transmutation
+     Problem
+
+    You are the most skilled alchemist of a country that considers metals such as gold, platinum, and silver to be uninteresting, but highly values lead. There are M metals known in the world; lead is metal number 1 on your periodic table. Your country's leader has asked you to use the metal in the treasury to make as much lead as possible.
+
+    For each metal (including lead), you know exactly one formula that lets you create one gram of that metal by destroying one gram each of two ingredient metals. (If you are wondering about the principle of mass conservation, the other gram is lost in useless waste products.) The formulas do not work with partial grams. However, you can use each formula as often as you would like (or not at all), as long as you have the required ingredients each time.
+
+    If you make optimal choices, what is the largest total amount of lead you can end up with? Note that it is possible that you may have some metals other than lead left over after you are done. 
+     */
     public static void Main(string[] args)
     {
     }
