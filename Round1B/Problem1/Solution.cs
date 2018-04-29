40ca3f34038982be3977f02c214627c6a624bf71
Jimmy Svejderud <jimmy@svejderud.se>
2018-04-29 19:38:28 +0200

    Round 1B: Problem 2 (WIP)

--- Round1B/Problem1/Solution.cs
+++ Round1B/Problem1/Solution.cs
@@ -6,18 +6,6 @@ using System.Threading.Tasks;
 
 public class Solution /* Problem 1 */
 {
-    /*
-    Rounding Error
-    Problem
-
-    To finally settle the age-old question of which programming language is the best, you are asking a total of N people to tell you their favorite language. This is an open-ended question: each person is free to name any language, and there are infinitely many languages in the world.
-
-    Some people have already responded, and you have gathered this information as a list of counts. For example, 1 2 means that you have asked 3 people so far, and one picked a particular language, and the other two picked some other language.
-
-    You plan to publish the results in a table listing each language and the percentage of people who picked it. You will round each percentage to the nearest integer, rounding up any percentage with a decimal part equal to or greater than 0.5. So, for example, 12.5% would round up to 13%, 99.5% would round up to 100%, and 12.4999% would round down to 12%.
-
-    In surveys like this, sometimes the rounded percentages do not add up to exactly 100. After you are done surveying the remaining people, what is the largest value that the rounded percentages could possibly add up to? 
-     */
     public static void Main(string[] args)
     {
         string testCasesInput = Console.ReadLine();
@@ -60,6 +48,7 @@ public class Solution /* Problem 1 */
                 {
                     var copy = it.ToList();
                     copy[t] += 1;
+                    // TODO : dont add duplicates
                     iterationVariations.Add(copy);
                 }
 
@@ -69,6 +58,8 @@ public class Solution /* Problem 1 */
                 iterationVariations.Add(copyForAdd);
             }
 
+            possibilities = null;
+            GC.Collect();
             possibilities = iterationVariations;
         }
 
--- Round1B/Problem2/Solution.cs
+++ Round1B/Problem2/Solution.cs
@@ -8,5 +8,67 @@ public class Solution /* Problem 2 */
 {
     public static void Main(string[] args)
     {
+        var totalTests = int.Parse(Console.ReadLine());
+
+        for (int t = 0; t < totalTests; t++)
+        {
+            var totalSigns = int.Parse(Console.ReadLine());
+            List<Sign> signs = new List<Sign>();
+            for (int s = 0; s < totalSigns; s++)
+            {
+                var signRow = Console.ReadLine();
+                var signSplit = signRow.Split(' ');
+                var sign = new Sign
+                {
+                    AtKm = int.Parse(signSplit[0]),
+                    West = int.Parse(signSplit[1]),
+                    East = int.Parse(signSplit[2]),
+                };
+                signs.Add(sign);
+            }
+        }
+    }
+
+    private static Tuple<int, int> Algorithm(List<Sign> signs)
+    {
+        List<List<int>> sequences = new List<List<int>>();
+
+        for (int i = 0; i < signs.Count; i++)
+        {
+            var s = signs[i];
+            // TODO
+        }
+
+        int longestSequence = sequences.Select(s => s.Count).Max();
+        int validSequencesCount = 0; // TODO : substract duplicates!
+        return new Tuple<int, int>(longestSequence, validSequencesCount);
+    }
+
+    private class Sequence
+    {
+        public int? M { get; set; }
+        public int? N { get; set; }
+        public List<int> Positions { get; set; }
+    }
+
+    private class Sign
+    {
+        public int AtKm { get; set; }
+        public int West { get; set; }
+        public int East { get; set; }
+
+        /*
+         D + A = M.
+         D - B = N. 
+        */
+        public int M
+        {
+            get { return AtKm + West; }
+        }
+
+        public int N
+        {
+            get { return AtKm - East; }
+        }
     }
 }
